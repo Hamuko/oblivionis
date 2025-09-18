5effeb39a7dc0661db8397d62ca96625f7346ca1
Hamuko <hamuko@burakku.com>
2025-09-18 22:30:14 +0300

    Add fallback mechanism for determining activity duration
    Some games like L.A. Noire seem to switch application IDs after starting
    the activity, which also makes the Discord API return a null start time
    for the activity when it ends. To counter this, we'll store the latest
    activity for the user in memory and use that if the normal start time is
    not available once the activity ends.

--- oblivionis/bot.py
+++ oblivionis/bot.py
@@ -15,10 +15,22 @@ intents.presences = True
 intents.members = True
 bot = commands.Bot(command_prefix=commands.when_mentioned, intents=intents)
 
+activities = {}
+
+
 def game_from_activity(activity) -> str:
     if activity.name == "Steam Deck":
         return activity.details.removeprefix("Playing ")
     return activity.name
+
+
+def get_stored_activity(member, activity) -> dict | None:
+    """Get an applicable activity from the in-memory storage."""
+    stored = activities.pop(member.id, None)
+    if stored and stored["application_id"] == activity.application_id:
+        return stored
+    return None
+
 
 @bot.event
 async def on_guild_available(guild):
@@ -33,8 +45,25 @@ async def on_presence_update(before, after):
 
     if after.activity is None and before.activity.type == discord.ActivityType.playing:
         activity = before.activity
-        duration = datetime.datetime.now(datetime.UTC) - activity.start
+
+        # Determine how long the activity lasted.
+        start = activity.start
+        if start is None and (stored_activity := get_stored_activity(after, activity)):
+            logger.warning(
+                "Discord API did not return activity start time; "
+                "falling back to stored value"
+            )
+            start = stored_activity["start"] or stored_activity["timestamp"]
+        if start is None:
+            logger.info(
+                "Could not determine duration for member %s activity %s",
+                after,
+                game_from_activity(activity),
+            )
+            return
+        duration = datetime.datetime.now(datetime.UTC) - start
         duration_seconds = round(duration.total_seconds())
+
         logger.info(
             "Member %s has stopped playing %s after %s seconds",
             after,
@@ -47,12 +76,28 @@ async def on_presence_update(before, after):
         if user_created:
             logger.info("Added new user '%s' to database", before.name)
 
-        game, game_created = storage.Game.get_or_create(name=game_from_activity(activity))
+        game, game_created = storage.Game.get_or_create(
+            name=game_from_activity(activity)
+        )
         if game_created:
             logger.info("Added new game '%s' to database", game.name)
         storage.Activity.create(user=user, game=game, seconds=duration_seconds)
     elif after.activity.type == discord.ActivityType.playing:
-        logger.info("Member %s has started playing %s", after, game_from_activity(after.activity))
+        if (
+            after.id not in activities
+            or activities[after.id]["application_id"] != after.activity.application_id
+        ):
+            activities[after.id] = {
+                "application_id": after.activity.application_id,
+                "name": after.activity.name,
+                "start": after.activity.start,
+                "timestamp": datetime.datetime.now(datetime.UTC),
+            }
+        logger.info(
+            "Member %s has started playing %s",
+            after,
+            game_from_activity(after.activity),
+        )
 
 
 @bot.event
